5155107afa015da6c2b310bc3ff9daff0f34d2ec
shaktimaan00 <97835032+shaktimaan00@users.noreply.github.com>
2022-07-06 12:51:25 +0530

    update footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -77,7 +77,7 @@ const Footer = () => {
                                                 variant='body2'
                                                 color={theme.palette.text.secondary}
                                             >
-                                                Copyright &copy; {new Date().getFullYear()} Bob's Programming Academy.
+                                                Copyright &copy; {new Date().getFullYear()} create by: @alone_shaktimaan.
                                             </Typography>
                                         }
                                     />
